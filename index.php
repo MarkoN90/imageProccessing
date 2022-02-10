bb11c9b7699472d73a3d09d2fa9735afd52920b9
MarkoNisevic <marko.nisevic@fayebsg.com>
2022-02-10 01:47:33 +0100

    whatever a

--- index.php
+++ index.php
@@ -2,12 +2,9 @@
 
 require_once 'vendor/autoload.php';
 
-foreach((new TesseractOCR())->availableLanguages() as $lang) echo $lang;
-exit;
-
 use thiagoalessio\TesseractOCR\TesseractOCR;
 echo (new TesseractOCR('example3.PNG'))
-->run();
+    ->run();
 
 
 
