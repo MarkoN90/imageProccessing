bb1703fd702297d0303379e337c566ea9d179f1f
MarkoNisevic <marko.nisevic@fayebsg.com>
2022-02-10 01:28:32 +0100

    try 3

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once 'vendor/autoload.php';
 
 use thiagoalessio\TesseractOCR\TesseractOCR;
-echo (new TesseractOCR('example1.png'))
+echo (new TesseractOCR('example3.PNG'))
     ->run();
 
 
