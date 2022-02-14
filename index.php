31090c3375f26fd1ba17ca306680c5e9a17d54af
MarkoNisevic <marko.nisevic@fayebsg.com>
2022-02-14 21:16:45 +0100

    reading pdf

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php 
 
+error_reporting(E_ALL);
+
 require_once 'vendor/autoload.php';
 
-use thiagoalessio\TesseractOCR\TesseractOCR;
-echo (new TesseractOCR('example3.PNG'))
-    ->run();
+use Spatie\PdfToText\Pdf;
+
+echo Pdf::getText('test.pdf'); //returns the text from the pdf
+
+
+
 
 
 
