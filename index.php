cfcdc9879cfdffe1a0362ea23ed31b95ec9aa2cf
MarkoNisevic <marko.nisevic@fayebsg.com>
2022-02-28 17:34:09 +0100

    multi structure file

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once 'vendor/autoload.php';
 
 use Spatie\PdfToText\Pdf;
 
-echo Pdf::getText('test.pdf'); //returns the text from the pdf
+echo Pdf::getText('test1.pdf'); //returns the text from the pdf
 
 
 
